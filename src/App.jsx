2dd5c29a691ff0e27049bc99397c1171188ee2f7
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-07-04 10:27:17 -0500

    feat: randomize on click

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function App() {
   const [clickedCountries, setClickedCountries] = useState([]);
   const [bestScore, setBestScore] = useState(0);
   const score = clickedCountries.length;
-  const countryData = [
+  const [countryData, setCountryData] = useState([
     { name: "United States", emoji: "🇺🇸" },
     { name: "Canada", emoji: "🇨🇦" },
     { name: "Mexico", emoji: "🇲🇽" },
     { name: "United Kingdom", emoji: "🇬🇧" },
-  ];
+  ]);
 
   if (score > bestScore) {
     setBestScore(score);
@@ -22,6 +22,7 @@ function App() {
       <Scoreboard score={score} bestScore={bestScore} />
       <Gameboard
         countryData={countryData}
+        setCountryData={setCountryData}
         clickedCountries={clickedCountries}
         setClickedCountries={setClickedCountries}
       />
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,30 +1,39 @@
 import Card from "./Card.jsx";
 
+function pureShuffle(data) {
+  const shuffledData = [...data];
+  for (let i = shuffledData.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledData[i], shuffledData[j]] = [shuffledData[j], shuffledData[i]];
+  }
+  return shuffledData;
+}
+
 export default function Gameboard({
   countryData,
+  setCountryData,
   clickedCountries,
   setClickedCountries,
 }) {
-  function updateList(name) {
+  function handleCardClick(name) {
     if (!clickedCountries.includes(name)) {
-      setClickedCountries((prev) => [...prev, name]);
+      setClickedCountries([...clickedCountries, name]);
     } else {
       setClickedCountries([]);
     }
+    setCountryData((prevData) => pureShuffle(prevData));
   }
 
   return (
-    <>
-      <section className="gameboard">
-        {countryData.map((country, index) => (
-          <Card
-            key={index}
-            name={country.name}
-            emoji={country.emoji}
-            onClick={() => updateList(country.name)}
-          />
-        ))}
-      </section>
-    </>
+    <section className="gameboard">
+      {countryData.map((country) => (
+        <Card
+          key={country.name}
+          name={country.name}
+          emoji={country.emoji}
+          onClick={() => handleCardClick(country.name)}
+        />
+      ))}
+    </section>
   );
 }
